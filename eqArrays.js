0290c24c02d4660f104ce5b90e54304d19e69bfe
Panos Hatzinikolaou <pjhatzis@gmail.com>
2019-11-27 04:35:43 +0000

    assertment whether 2 arrays are equal

---
+++ eqArrays.js
@@ -0,0 +1,29 @@
+const assertEqual = function(actual, expected){
+  if (actual === expected){
+    console.log(`😀😀😀 ${actual} === ${expected}`);
+  } else {
+    console.log(`😟😟😟 ${actual} !== ${expected}`);
+  }
+}
+
+const eqArrays = function(array1, array2){
+  let assertment = true;
+  if (array1.length === array2.length){
+    for (let i = 0; i < array1.length; i++){
+      if (array1[i] === array2[i] && typeof array1[i] === typeof array2[i]){
+        assertment = true;
+      } else if (array1[i] !== array2[i]) {
+        assertment = false;
+      };
+    }
+  }
+  return assertment;
+}
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => true
+assertEqual(eqArrays([1, 2, 3], [3, 2, 1]), false); // => false
+
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", "3"]), true); // => true
+assertEqual(eqArrays(["1", "2", "3"], ["1", "2", 3]), false); // => false
