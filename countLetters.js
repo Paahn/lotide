dc2706eb2a35fcd3b67eb85bc60c4129ebafe4d0
Panos Hatzinikolaou <pjhatzis@gmail.com>
2019-11-28 01:44:03 +0000

    this file counts the number of letters that occur in a string

--- countLetters.js
+++ countLetters.js
@@ -1,27 +1,31 @@
-const assertEqual = function(actual, expected){
-  if (actual === expected){
-    console.log(`😀😀😀 ${actual} === ${expected}`);
-  } else {
-    console.log(`😟😟😟 ${actual} !== ${expected}`);
-  }
-}
-// const countObj = { counter: 0 }
+// What are you doing here assertEqual function???????????
+// const assertEqual = function(actual, expected){
+//   if (actual === expected){
+//     console.log(`😀😀😀 ${actual} === ${expected}`);
+//   } else {
+//     console.log(`😟😟😟 ${actual} !== ${expected}`);
+//   }
+// }
 
-const countLetters = function(inputString, checkString){
-  let counter = 0;
-  for (const letter of inputString){
-    for (const otherLetter of checkString){
-      if (letter === otherLetter){
-        counter += 1;
+
+const countLetters = function (inputString) {
+  const results = {};
+
+  for (const item of inputString) {
+    if (results[item]) {
+        results[item] += 1;
+      } else {
+        results[item] = 1;
       }
-    }
   }
-  return counter;
+  return results;
 }
-assertEqual(countLetters("alice in navy", "a"), 2);
-assertEqual(countLetters("alice in navy", "alvy"), 5);
-assertEqual(countLetters("Super Van Vacation vortex k?", "vuk"), 3);
-assertEqual(countLetters("aaa", "asd"), 3);
-assertEqual(countLetters("Super Van Vacation vortex k?", "b"), 0);
+console.log(countLetters("lighthouse in the house"));
+console.log(countLetters("zavarakatranemia"));
+
+
+
+
+
 
 
