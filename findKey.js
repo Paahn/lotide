ac1b5d322f55ca426cfa90b45883b0bb1204ad39
Panos Hatzinikolaou <pjhatzis@gmail.com>
2019-11-29 07:00:06 +0000

    findKey takes in an object and a callback function and returns the key when the callback condition is met

---
+++ findKey.js
@@ -0,0 +1,21 @@
+const assertEqual = function(actual, expected){
+  if (actual === expected){
+    console.log(`😀😀😀 ${actual} === ${expected}`);
+  } else {
+    console.log(`😟😟😟 ${actual} !== ${expected}`);
+  }
+}
+
+const findKey = (obj, callback) => Object.keys(obj).find(key => callback(obj[key]));
+
+const youLookForIt = findKey({
+  "Blue Hill": { stars: 1 },
+  "Akaleri":   { stars: 3 },
+  "noma":      { stars: 2 },
+  "elBulli":   { stars: 3 },
+  "Ora":       { stars: 2 },
+  "Akelarre":  { stars: 3 }
+}, x => x.stars === 2)
+console.log(youLookForIt);
+
+assertEqual(youLookForIt, "noma");
