0e8682361430478bbab75f080548001fc4e6c4fa
Panos Hatzinikolaou <pjhatzis@gmail.com>
2019-11-29 08:20:45 +0000

    the flattening array function

---
+++ flatten.js
@@ -0,0 +1,39 @@
+const eqArrays = function(array1, array2){
+  let assertment = true;
+  if (array1.length === array2.length){
+    for (let i = 0; i < array1.length; i++){
+      if (array1[i] === array2[i] && typeof array1[i] === typeof array2[i]){
+        assertment = true;
+      } else if (array1[i] !== array2[i]) {
+        assertment = false;
+      };
+    }
+  } else {
+    return false;
+  }
+  return assertment;
+}
+
+const assertArraysEqual = function(list1, list2){
+  console.log(eqArrays(list1, list2));
+   if (eqArrays(list1, list2)){
+    console.log(`😀😀😀 ${list1} === ${list2}`);
+   } else {
+    console.log(`😟😟😟 ${list1} !== ${list2}`)
+   }
+}
+
+const flatten = function(array) {
+  let flattened = [];
+  for (let i in array) {
+    if (Array.isArray(array[i])) {
+      for (let j in array[i]) {
+        flattened.push(array[i][j]);
+      }
+    } else {
+      flattened.push(array[i]);
+    }
+  } return flattened;
+};
+
+assertArraysEqual(flatten([1, 2, [3, 4], 5, [6]]), [1, 2, 3, 4, 5, 6]); // => [1, 2, 3, 4, 5, 6]
