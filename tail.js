2ce8c16a961cad7a7eaa7b2ef9478ccdc843e6e5
Panos Hatzinikolaou <pjhatzis@gmail.com>
2019-12-02 21:24:49 +0000

    made test for tail.js and refactored to implement my new found power with modules

--- tail.js
+++ tail.js
@@ -1,10 +1,3 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`😀😀😀 ${actual} === ${expected}`);
-  } else {
-    console.log(`😟😟😟 ${actual} !== ${expected}`);
-  }
-};
 const tail = function(array) {
   let newArray = [];
   for (let i = 0; i <= array.length; i++) {
@@ -14,12 +7,7 @@ const tail = function(array) {
   }
   return newArray;
 };
-assertEqual("Lighthouse Labs", "Bootcamp");
-assertEqual(1, 1);
-assertEqual("hello Kitty", "hello Kitty");
-assertEqual(32, 337);
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+
+
+module.exports = tail;
 
---
+++ test/tailTest.js
@@ -0,0 +1,11 @@
+const assertEqual = require('../assertEqual')
+const tail = require('../tail')
+
+assertEqual("Lighthouse Labs", "Bootcamp");
+assertEqual(1, 1);
+assertEqual("hello Kitty", "hello Kitty");
+assertEqual(32, 337);
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
