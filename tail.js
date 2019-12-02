537d4762347b2207fe099797b8458f699d6bcaf2
Panos Hatzinikolaou <pjhatzis@gmail.com>
2019-12-02 23:25:02 +0000

    updated bug in tail.js, now tail is returning all elements of the array but the first

--- tail.js
+++ tail.js
@@ -1,13 +1,8 @@
 const tail = function(array) {
-  let newArray = [];
-  for (let i = 0; i <= array.length; i++) {
-    if (i !== 0) {
-      newArray = array[i];
-    }
-  }
+  let newArray = array.slice(1);
   return newArray;
 };
-
+console.log(tail([3, 6, 7, 8]));
 
 module.exports = tail;
 
