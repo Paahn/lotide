87fd82e19e79e82afb59846205cc3e8b1aece7d4
Panos Hatzinikolaou <pjhatzis@gmail.com>
2019-12-02 22:01:49 +0000

    added comments to eqArrays to better explain what it does

--- eqArrays.js
+++ eqArrays.js
@@ -1,8 +1,10 @@
 const assertEqual = require('./assertEqual')
 
+// eqArrays checks if two arrays are identical
 
 const eqArrays = function(array1, array2){
   let assertment = true;
+  // guard condition ie if the arrays dont have the same length, they're not equal
   if (array1.length === array2.length){
     for (let i = 0; i < array1.length; i++){
       if (array1[i] === array2[i] && typeof array1[i] === typeof array2[i]){
