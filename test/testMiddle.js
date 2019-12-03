da285d02b4c3a22a69274e4ee07944d69182984c
Panos Hatzinikolaou <pjhatzis@gmail.com>
2019-12-03 00:21:17 +0000

    implemented testMiddle.js with assert.deepEqual to fully take advantage of chai

--- test/testMiddle.js
+++ test/testMiddle.js
@@ -12,14 +12,14 @@ const assert = require('chai').assert;
 
 describe("#middle", () => {
   it("should return the middle two items of an even length array", () => {
-    assert.strictEqual(assertArraysEqual(middle([1, 2, 3, 4]), [2, 3]));
+    assert.deepEqual(middle([1, 2, 3, 4]), [2, 3]);
   });
 
   it("should return the middle element of an odd length array", () => {
-    assert.strictEqual(assertArraysEqual(middle([1, 2, 3, 4, 5]), [3]));
+    assert.deepEqual(middle([1, 2, 3, 4, 5]), [3]);
   });
   
   it("should return the same array when the length is smaller or equal to 2", () => {
-    assert.strictEqual(assertArraysEqual(middle([1, 2]), [1, 2]));
+    assert.deepEqual(middle([1, 2]), [1, 2]);
   });
 });
